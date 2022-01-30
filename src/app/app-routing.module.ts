17a2a5bafe36a75321d69ed3e2e583ad32d48d5f
Sofi­a Schenone <sofiainesschenone@gmail.com>
2022-01-30 19:33:29 -0300

    Se hicieron las rutas y la imagen de home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
+import { PokeTableComponent } from './components/poke-table/poke-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: PokeTableComponent},
+  {path:'pokeDetail/:id', component: PokeDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+ home(){
+  this.router.navigateUrl('/home');
+ }
+}
