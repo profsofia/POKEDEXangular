3c9af438023ed29f8bcfdea3536e102aa2c420a2
Sofi­a Schenone <sofiainesschenone@gmail.com>
2022-02-03 10:40:07 -0300

    cambios para produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,3 +15,7 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+export const environment = {
+  production: true,
+  baseUrl: 'https://pokeapi.co/api/v2'
+};
